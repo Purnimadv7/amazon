4b2938804e06f5cccebab238fb060c37d87d2dad
Purnima <purnimadv7@gmail.com>
2020-04-15 13:53:35 +0000

    second commit

---
+++ argclgs.sh
@@ -0,0 +1,7 @@
+echo addition of two numbers is : `expr $1 + $2`
+echo substraction od two numbers is : 'expr $1 - $2'
+echo mul of two number is : 'expr $1 * $2'
+echo div of two numbers is : 'expr $1 % $2'
+
+
+
